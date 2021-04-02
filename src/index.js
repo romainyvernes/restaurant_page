aa255ea16c73f15a481705c5bc1d843b83d9bf94
Romain <romainyvernes@gmail.com>
2021-04-02 16:08:15 -0700

    Page switching completed

--- src/index.js
+++ src/index.js
@@ -4,28 +4,39 @@ import contact from './contact'
 
 function clearContainer() {
     const container = document.querySelector('#content');
-    // loop through child nodes until no child node
+    let firstChild = container.firstElementChild;
+
+    while (firstChild) {
+        firstChild.remove();
+        firstChild = container.firstElementChild;
+    }
+}
+
+function assignEvents() {
+    const homeBtn = document.querySelector('#home-btn');
+    const menuBtn = document.querySelector('#menu-btn');
+    const contactBtn = document.querySelector('#contact-btn');
+
+    homeBtn.addEventListener('click', (event) => {
+        clearContainer();
+        homepage.render();
+        assignEvents();
+    });
+
+    menuBtn.addEventListener('click', (event) => {
+        clearContainer();
+        menu.render();
+        assignEvents();
+    });
+
+    contactBtn.addEventListener('click', (event) => {
+        clearContainer();
+        contact.render();
+        assignEvents();
+    });
 }
 
 window.onload = (event) => {
     homepage.render();
+    assignEvents();
 };
-
-const homeBtn = document.querySelector('#home-btn');
-const menuBtn = document.querySelector('#menu-btn');
-const contactBtn = document.querySelector('#contact-btn');
-
-homeBtn.addEventListener('click', (event) => {
-    clearContainer();
-    homepage.render();
-})
-
-menuBtn.addEventListener('click', (event) => {
-    clearContainer();
-    homepage.render();
-})
-
-contactBtn.addEventListener('click', (event) => {
-    clearContainer();
-    homepage.render();
-})
